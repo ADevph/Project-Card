f2d97a0c61fb81b9f48108441778f73d3fb0bbd9
ADevph <das.atanu42914@gmail.com>
2024-02-10 12:10:14 +0600

    search functionality complete

--- src/components/Userlist.jsx
+++ src/components/Userlist.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import UserCard from './UserCard';
 
-const Userlist = () => {
+const Userlist= () => {
   const [users, setUsers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     fetch('https://dummyjson.com/users')
@@ -11,13 +12,31 @@ const Userlist = () => {
       .catch(error => console.error('Error fetching users:', error));
   }, []);
 
+  const filteredUsers = users.filter(user => {
+    const fullName = `${user.firstName} ${user.lastName}`.toLowerCase();
+    return fullName.includes(searchQuery.toLowerCase());
+  });
+
+  const handleSearchChange = event => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
-    <div className="flex flex-wrap justify-center p-4">
-      {users.map(user => (
-        <div key={user.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-2">
-          <UserCard user={user} />
-        </div>
-      ))}
+    <div className="p-4">
+      <input
+        type="text"
+        placeholder="Search by name"
+        value={searchQuery}
+        onChange={handleSearchChange}
+        className="w-full p-2 mb-4 border rounded-md"
+      />
+      <div className="flex flex-wrap justify-center">
+        {filteredUsers.map(user => (
+          <div key={user.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-2">
+            <UserCard user={user} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
